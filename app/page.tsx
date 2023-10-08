f76cdbee4e87348b14b4450deeda7300687d5174
Jan Kuźma <134098590+jankuzma@users.noreply.github.com>
2023-10-08 17:50:25 +0200

    clerk setup and initial main page ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 import Image from 'next/image'
 import {Button} from "@/components/ui/button";
+import {auth, UserButton} from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+
+
+    const {userId} = await auth()
+    const isAuth = !!userId
+
+
   return (
-<div>
-  <Button>
-    Click me !
-  </Button>
-</div>
+    <div className={'w-screen min-h-screen from-gray-900 to-gray-600 bg-gradient-to-r'}>
+      <div className={'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'}>
+        <div className="flex flex-col items-center text-center ">
+            <div className="flex items-center">
+                <h1 className={'mr-3 text-5xl font-semibold text-slate-300'}>Chat with any pptx</h1>
+                <UserButton afterSignOutUrl={'/'} />
+            </div>
+            <div className="flex mt-2">
+                {isAuth && <Button>Go to chats!</Button>}
+            </div>
+            <p className={'max-w-xl mt-1 text-lg text-slate-400'}>
+                join millions of students that save hours of notes re-writing
+            </p>
+        </div>
+      </div>
+    </div>
   )
 }
